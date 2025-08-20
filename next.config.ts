26b32c2ea3a5d70271c134df3a2e78a0069091eb
Ali Molhem <a.molhem@hardwework.com>
2025-08-20 15:35:58 +0400

    next config modification

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   assetPrefix: "/blog-static",
+  output: "standalone",
 };
 
 export default nextConfig;
