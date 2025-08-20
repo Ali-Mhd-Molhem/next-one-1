ac0a4e3d78bf7e2e78d510528c0c313f63d0cfed
Ali Molhem <a.molhem@hardwework.com>
2025-08-20 14:38:09 +0400

    initial

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Next.js - Blog Zone",
+  description: "Next.js example for Multi Zones",
+};
+
+export default function BlogLAyout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
